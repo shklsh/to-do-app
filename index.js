990b5e73438a45de3fe51ff7e835c8e52f14019e
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-09-16 07:18:42 +1000

    16/09/19

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-document.addEventListener("click", function () {
-    document.getElementById("myUL").appendChild(li);
-});
+//Create a new list when "Add" button clicked
+function addList() {
+    let list = document.createElement("li");
+    let inputValue = document.getElementById("myText").value;
+    let text = document.createTextNode(inputValue);
+    list.appendChild(text);
+    if (inputValue === '') {
+        alert("Type something before adding a list!");
+    } else {
+        document.getElementById("myList").appendChild(list);
+    }
+    document.getElementById("myText").value = "";
+}
+
+//Edit an existing list when "Edit" button clicked
+let li = [];
+let i = li;
+let list1 = document.querySelector("ul");
+let editList = document.getElementById("#editButton[i]");
+for (i=0;i<li.length;i++) {
+    editList.onclick = function() {
+        list1.contentEditable = true;
+    }
+}
+
+//Delete an existing list when "Delete" button clicked
