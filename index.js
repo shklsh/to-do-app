f9e5cf0103cf27f60354dc70ccb84143d9ba661c
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-09-17 18:06:48 +1000

    170919

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-//Create a new list when "Add" button clicked
-function addList() {
-    let list = document.createElement("li");
-    let inputValue = document.getElementById("myText").value;
-    let text = document.createTextNode(inputValue);
-    list.appendChild(text);
-    if (inputValue === '') {
-        alert("Type something before adding a list!");
-    } else {
-        document.getElementById("myList").appendChild(list);
-    }
-    document.getElementById("myText").value = "";
+const listItemTemplate = `
+	<li>
+        <input type="text" value="taskName">
+        <input type="date" value="taskDate">
+        <input type="button" value="Edit">
+        <input type="button" value="Delete">
+        <input type="checkbox" value="" checked>
+    </li>
+`;
+
+let tasks = [];
+
+function addTask(taskName, taskDate) {
+    tasks.push({
+        name: taskName,
+        dueDate: taskDate,
+    });
 }
 
-//Edit an existing list when "Edit" button clicked
-let li = [];
-let i = li;
-let list1 = document.querySelector("ul");
-let editList = document.getElementById("#editButton[i]");
-for (i=0;i<li.length;i++) {
-    editList.onclick = function() {
-        list1.contentEditable = true;
+//Create a new list when "Add" button clicked
+function addList() {
+    let inputValue = $("#myText").val();
+    let inputDate = $("#myDate").val();
+
+    if (inputValue !== '') {
+        let liText = listItemTemplate;
+        liText = liText.replace("taskName", inputValue); 
+        liText = liText.replace("taskDate", inputDate); 
+        $("#myList").append(liText);
+        addTask(inputValue, inputDate);
+        console.log(tasks);
     }
 }
+
+//Edit 
 
 //Delete an existing list when "Delete" button clicked
