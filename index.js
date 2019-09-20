20e9c70e978a1e348688a99284f5096e8216948e
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-09-21 06:26:06 +1000

    210919

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 const listItemTemplate = `
 	<li>
-        <input type="text" value="taskName">
-        <input type="date" value="taskDate">
+        <input type="checkbox" class="checkBox">    
+        <span class="newTaskName"></span>
+        <input type="date" class="newTaskDate">
+        <input type="button" class="editButton" value="Edit">
         <input type="button" class="deleteButton" value="Delete">
-        <input type="checkbox">
     </li>
 `;
 
 let tasks = [];
 
-function addTask(taskName, taskDate) {
+function addTask(newTaskName, newTaskDate) {
     tasks.push({
-        name: taskName,
-        dueDate: taskDate,
+        name: newTaskName,
+        dueDate: newTaskDate,
+        //isDone: false
     });
 }
 
 //Create a new list when "Add" button clicked
 function addList() {
-    let inputValue = $("#myText").val();
-    let inputDate = $("#myDate").val();
+    let inputValue = $("#myNewToDoListText").val();
+    let inputDate = $("#myNewToDoListDate").val();
 
     if (inputValue !== '') {
         let liText = listItemTemplate;
-        liText = liText.replace("taskName", inputValue); 
-        liText = liText.replace("taskDate", inputDate); 
+        // liText = liText.replace(inputValue); 
+        // liText = liText.replace(inputDate); 
         $("#myList").append(liText);
+        $('#myList > li:last-child > span').text(inputValue);
+        $('#myList > li:last-child > input [type="date"]').attr(inputDate);
         addTask(inputValue, inputDate);
-        console.log(tasks);
     }
 }
+//Edit an existing list when "Edit" button clicked
+$('.editButton').click(function () {
+    $('li').prop('contenteditable', 'true');
+})
 
 //Delete an existing list when "Delete" button clicked
 $(document).on('click', '.deleteButton', function() {
     $(this).parent().remove();
+    //update the array
 })
