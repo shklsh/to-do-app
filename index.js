6e422d075b09b56a11eb34fee85d721eb248645f
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-09-21 10:26:25 +1000

    210919

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const listItemTemplate = `
 let tasks = [];
 
 function addTask(newTaskName, newTaskDate) {
+    //Update task array
     tasks.push({
         name: newTaskName,
         dueDate: newTaskDate,
@@ -29,7 +30,7 @@ function addList() {
         // liText = liText.replace(inputDate); 
         $("#myList").append(liText);
         $('#myList > li:last-child > span').text(inputValue);
-        $('#myList > li:last-child > input [type="date"]').attr(inputDate);
+        $('#myList > li:last-child > input [type=date]').attr(inputDate);
         addTask(inputValue, inputDate);
     }
 }
